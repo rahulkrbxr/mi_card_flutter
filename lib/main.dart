fdd225fba9e33cca1084858bda7a469f5d5e0f94
rahulkrbxr <rahulkumarb4ubxr@gmail.com>
2021-05-27 04:37:19 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                // backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/face.jpg'),
               ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow.shade50,
-                    )
-                  ],
+              Text(
+                'John Doe',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              Text(
+                'EXECUTIVE MANAGER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                // color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+1 648 576 2353',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                // color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'johndoe@merrywhether.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -51,29 +92,54 @@ class MyApp extends StatelessWidget {
 }
 
 // Row(
-// // mainAxisSize: MainAxisSize.min,
-// // verticalDirection: VerticalDirection.up,
-// // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-// crossAxisAlignment: CrossAxisAlignment.stretch,
+// children: [
+// Icon(
+// Icons.email,
+// color: Colors.teal,
 //
+// ),
+// SizedBox(
+// width: 10.0,
+// ),
+// Text(
+// 'johndoe@merrywhether.com',
+// style: TextStyle(
+// color: Colors.teal,
+// fontFamily: 'Source Sans Pro',
+// fontSize: 20.0,
+// ),
+// ),
+// ],
+// ),
+
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// crossAxisAlignment: CrossAxisAlignment.stretch,
 // children: [
 // Container(
-// width: 30.0,
-// // height: 100.0,
-// color: Colors.white,
-// child: Text('Container 1'),
-// ),
-// Container(
 // width: 100.0,
-// // height: 100.0,
-// color: Colors.blue,
-// child: Text('Container 2'),
-// ),
-// Container(
-// width: 100.0,
-// // height: 100.0,
 // color: Colors.red,
-// child: Text('Container 3'),
+// ),
+// Container(
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Container(
+// width: 100.0,
+// height: 100.0,
+// color: Colors.yellow,
+// ),
+// Container(
+// width: 100.0,
+// height: 100.0,
+// color: Colors.yellow.shade50,
+// )
+// ],
+// ),
+// ),
+// Container(
+// width: 100.0,
+// color: Colors.blue,
 // ),
 // ],
 // ),
